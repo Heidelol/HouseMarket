bd339912e7d24812d7db9812cac76b3bd42201ea
Heidelol <heideloliver@gmail.com>
2022-06-15 22:48:28 -0500

    add

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React,{useState, useEffect, useRef} from 'react'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import {useNavigate} from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function CreateListing() {
+    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+    const [formData, setFormData] = useState({
+        type: 'rent',
+        name: '',
+        bedrooms: 1,
+        bathroom: 1,
+        parking: false,
+        furnished: false,
+        address: '',
+        offer: false,
+        regularPrice: 0,
+        discountedPrice: 0,
+        images: {},
+        latitude: 0,
+        longitude: 0
+    })
+
+    const auth = getAuth()
+    const navigate = useNavigate()
+    const isMounted = useRef(true)
+
+    useEffect(() => {
+        if(isMounted){
+            onAuthStateChanged(auth, (user) => {
+                if(user) {
+                    setFormData({...formData, userRef: user.uid})
+                } else {
+                    navigate('/sign-in')
+                }
+            })
+        }
+
+        return () => {
+            isMounted.current = false
+        }
+    }, [isMounted])
+
+
   return (
-    <div>CreateListing</div>
+    <div>CreateListing1</div>
   )
 }
 
